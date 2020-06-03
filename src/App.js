fcd8b1c1037d1adb9e0844992104e9ff1351ea25
moeti <moetichentuf@hotmail.com>
2020-06-03 13:10:21 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
 import React from 'react';
-import logo from './logo.svg';
+import styled from 'styled-components';
 import './App.css';
+const Button = styled.button`
+  cursor: pointer;
+  background: transparent;
+  font-size: 16px;
+  border-radius: 3px;
+  color: palevioletred;
+  border: 2px solid palevioletred;
+  margin: 0 1em;
+  padding: 0.25em 1em;
+  transition: 0.5s all ease-out;
+ 
+  &:hover {
+    background-color: palevioletred;
+    color: white;
+  }
+`;
+const Input = styled.input.attrs(props => ({
+  // we can define static props
+  type: "password",
 
+  // or we can define dynamic ones
+  size: props.size || "1em",
+}))`
+  color: palevioletred;
+  font-size: 1em;
+  border: 2px solid palevioletred;
+  border-radius: 3px;
+
+  /* here we use the dynamically computed prop */
+  margin: ${props => props.size};
+  padding: ${props => props.size};
+`;
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Input></Input>
+      <h1>Styled Components Test</h1>
+      
+      <Button>I am a Button</Button>
     </div>
   );
 }
-
+ 
 export default App;
