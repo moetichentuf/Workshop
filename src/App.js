945806aefb0dca83b9dc8c2c52addf88be012cec
moeti <moetichentuf@hotmail.com>
2020-06-03 13:26:01 +0200

    adding animation examples

--- src/App.js
+++ src/App.js
@@ -1,6 +1,30 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components'
 import './App.css';
+const breatheAnimation = keyframes`
+ 0% { height: 100px; width: 100px; }
+ 30% { height: 400px; width: 400px; opacity: 1 }
+ 40% { height: 405px; width: 405px; opacity: 0.3; }
+ 100% { height: 100px; width: 100px; opacity: 0.6; }
+`
+const Circle = styled.div`
+ height: 100px;
+ width: 100px;
+ border-style: solid;
+ border-width: 5px;
+ border-radius: 50%;
+ border-color: black;
+ animation-name: ${breatheAnimation};
+ animation-duration: 8s;
+ animation-iteration-count: infinite;
+`
+const Container = styled.div`
+ display: flex;
+ align-items: center;
+ justify-content: center;
+ flex-direction: column;
+ height: 450px;
+`
 const Button = styled.button`
   cursor: pointer;
   background: transparent;
@@ -36,6 +60,9 @@ const Input = styled.input.attrs(props => ({
 function App() {
   return (
     <div>
+      <Container>
+   <Circle />
+  </Container>
       <Input></Input>
       <h1>Styled Components Test</h1>
       
