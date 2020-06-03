194703b29b15c4086923af6e6294b39c60d28b81
moeti <moetichentuf@hotmail.com>
2020-06-03 22:29:51 +0200

    Updates

---
+++ precache-manifest.4b88f021f41be7b6a544fe9d915fb6b5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0487e037ab613fec4a2096dd9f210a7",
+    "url": "/Workshop/index.html"
+  },
+  {
+    "revision": "91cd5fce4630b407a4a7",
+    "url": "/Workshop/static/css/main.45543fd6.chunk.css"
+  },
+  {
+    "revision": "f24095299ef5b298a27d",
+    "url": "/Workshop/static/js/2.9020094e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Workshop/static/js/2.9020094e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91cd5fce4630b407a4a7",
+    "url": "/Workshop/static/js/main.3fe581a9.chunk.js"
+  },
+  {
+    "revision": "1a0fef97ec4363c94061",
+    "url": "/Workshop/static/js/runtime-main.2b0ef76e.js"
+  }
+]);
